75f97b9401ca152b117ed71c66939092c337e848
Aselemi Divine <aselemidivine@gmail.com>
2023-05-19 17:02:20 +0100

    created the global state functions with redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homepage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
+
 
 function App() {
-  return <div className="App"></div>;
+
+  return <div className="App">
+    {/* SETTING OUR ROUTES */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/profile/:userId" element={<ProfilePage />} />
+
+      </Routes>
+    </BrowserRouter>
+  </div>;
 }
 
 export default App;
---
+++ client/src/state/index.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// This state will be stored in our global state and it will be accessible throughout the application.
+const initialState = {
+  mode: "light",
+  user: null,
+  token: null, // Store all the auth information
+  posts: [],
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.error("user friends non-existent :( ");
+      }
+    },
+  },
+});
