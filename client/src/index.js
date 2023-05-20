afa710758adeae6e550664eb9ed1499998ddc57d
Aselemi Divine <aselemidivine@gmail.com>
2023-05-21 00:20:37 +0100

    added the redux-persist in the index file

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import authReducer from './state';
+import { configurateStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import {
+  persistentStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+// Even tho the browser is closed, The user information and data would still be stored in the browser.
+
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => 
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [ FLUSH, REHYDRATE, PAUSE, REGISTER, PURGE, PERSIST ],
+    },
+  }),
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -31,5 +31,19 @@ export const authSlice = createSlice({
         console.error("user friends non-existent :( ");
       }
     },
+    setPosts: (state, action) => {
+      state.posts = action.payload.posts;
+    },
+    setPost: (state, action) => {
+      const updatedPosts = state.posts.map((post) => {
+        if (post._id === action.payload.post_id) return action.payload.post;
+        return post;
+      });
+      state.posts = updatedPosts;
+    },
   },
 });
+
+export const { setMode, setPosts, setLogin, setLogout, setFriends, setPost } =
+  authSlice.actions;
+export default authSlice.reducer;
